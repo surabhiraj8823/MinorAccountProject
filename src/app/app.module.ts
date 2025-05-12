23ea2f7cdb5197589fc283051f615376607c647c
Varun sai V <61574888+Varun-Sai2003@users.noreply.github.com>
2025-05-12 20:21:30 +0530

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { RegisterComponent } from './register/register.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    NavigationComponent,
+    RegisterComponent,
+    DashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
+    CommonModule,
+    RouterModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/register/register.component.ts
@@ -0,0 +1,112 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { forkJoin } from 'rxjs';
+import { LoginService } from '../services/login.service';
+import { Address } from '../models/address';
+
+@Component({
+  selector: 'app-register',
+  standalone: false,
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent {
+  countries: { name: string; states: { name: string; cities: string[] }[] }[] = [];
+  states: { name: string; cities: string[] }[] = [];
+  cities: string[] = [];
+
+  showPassword: boolean = false;
+  registerForm: FormGroup;
+  public customerId: string;
+  public formId: number;
+
+  constructor(public fb: FormBuilder, public loginService: LoginService) {
+    this.customerId = this.generateCustomerId();
+    this.formId = this.generateFormId();
+
+    this.registerForm = this.fb.group({
+      form_id: [this.formId],
+      first_name: ['', Validators.required],
+      middle_name: [''],
+      last_name: ['', Validators.required],
+      email_id: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(15)]],
+      address: this.fb.group({
+        type: ['', Validators.required],
+        line1: ['', Validators.required],
+        line2: ['', Validators.required],
+        line3: [''],
+        city: ['', Validators.required],
+        state: ['', Validators.required],
+        country: ['', Validators.required],
+        pin_code: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(6)]],
+        phone_number: ['', [Validators.required, Validators.pattern('\\d{10}')]]
+      }),
+      addressProof: this.fb.group({
+        type: [''],
+        document_number: [''],
+        aadhar_number: ['', [Validators.pattern('\\d{12}')]],
+        pan_number: ['', [Validators.pattern('[A-Z]{5}[0-9]{4}[A-Z]{1}')]]
+      })
+    });
+
+    this.loadCountries();
+  }
+
+  generateCustomerId(): string {
+    return Math.floor(100000 + Math.random() * 900000).toString();
+  }
+
+  generateFormId(): number {
+    return Math.floor(1000 + Math.random() * 9000);
+  }
+
+  loadCountries() {
+    this.loginService.getCountries().subscribe((data: any) => {
+      this.countries = data;
+    });
+  }
+
+  onCountryChange(event: any) {
+    const selectedCountry = event.target.value;
+    const country = this.countries.find(c => c.name === selectedCountry);
+    this.states = country ? country.states : [];
+    this.cities = []; // Reset cities when country changes
+    this.registerForm.get('address.state')?.reset();
+    this.registerForm.get('address.city')?.reset();
+  }
+
+  onStateChange(event: any) {
+    const selectedState = event.target.value;
+    const state = this.states.find(s => s.name === selectedState);
+    this.cities = state ? state.cities : [];
+    this.registerForm.get('address.city')?.reset();
+  }
+
+  submitForm() {
+    if (this.registerForm.valid) {
+      const customerId = this.customerId;
+      const formId = this.formId;
+
+      const customerData = {
+        form_id: formId.toString(),
+        customer_id: customerId,
+        ...this.registerForm.value
+      };
+
+      forkJoin({
+        customer: this.loginService.addUserdetails(customerData),
+        address: this.loginService.addAddress(customerData.address),
+        addressProof: this.loginService.addAddressproof(customerData.addressProof)
+      }).subscribe(response => {
+        console.log("Data saved successfully:", response);
+        alert("User registered successfully!");
+        this.resetForm();
+      });
+    }
+  }
+
+  resetForm(): void {
+    this.registerForm.reset();
+  }
+}
