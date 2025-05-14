9664f3cab5fddfd57a14e1c21b383c81a8415263
Varun sai V <61574888+Varun-Sai2003@users.noreply.github.com>
2025-05-14 13:23:06 +0530

    Delete src directory

--- src/app/models/user.ts
+++
@@ -1,9 +0,0 @@
-export class User {
-    form_id: string = '';
-    customer_id: string = '';
-    first_name: string = '';
-    middle_name: string = '';
-    last_name: string = '';
-    email_id: string = '';
-    password: string = '';
-}
